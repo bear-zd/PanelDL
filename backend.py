6a825dc7d996de72f8ab2b8fdb803b83a60777d1
Zidea <a827569457@163.com>
2022-02-08 00:12:19 +0800

    add database password authorize

--- backend.py
+++ backend.py
@@ -4,29 +4,32 @@ from flask import Flask, request, redirect, render_template, session
 from werkzeug.serving import run_simple
 # import dash_html_components as html
 from dash import html
+from PanelDL.utils.private import SECRET_KEY
+from PanelDL import easySql
 
 
 server = Flask(__name__)
+server.secret_key = SECRET_KEY
 from app import userProject
 menu = userProject(server)
+query = easySql.sql_query()
+
 #dash_app1 = Dash(__name__, server=server, url_base_pathname='/dashboard/')
 #dash_app2 = Dash(__name__, server=server, url_base_pathname='/reports/')
 #dash_app1.layout = html.Div([html.H1('Hi there, I am app1 for dashboards')])
 #dash_app2.layout = html.Div([html.H1('Hi there, I am app2 for reports')])
 
 
-@server.route('/')
-def redirect():
-    return redirect('login')
-
-@server.route('/login', methods=["GET", "POST"])  # 路由默认接收请求方式位POST，然而登录所需要请求都有，所以要特别声明。
+@server.route('/', methods=["GET", "POST"], endpoint='/')
+@server.route('/login', methods=["GET", "POST"], endpoint='/login')  # 路由默认接收请求方式位POST，然而登录所需要请求都有，所以要特别声明。
 def login():
     if request.method == 'GET':
         return render_template('login.html')
     user = request.form.get('user')
     pwd = request.form.get('pwd')
-    if user == 'admin' and pwd == '123':  # 这里可以根据数据库里的用户和密码来判断，因为是最简单的登录界面，数据库学的不是很好，所有没用。
-        # session['user_info'] = user
+    global query
+    if query.get_user_id(user, pwd) != None:
+        session['user_info'] = user
         return redirect('/menu')
     else:
         return render_template('login.html', msg='用户名或密码输入错误')
@@ -39,7 +42,10 @@ def ping():
 
 @server.route('/menu')
 def render_dashboard():
-    return redirect('/dash1')
+    if session.get('user_info') == None:
+        return redirect('/login')
+    else:
+        return redirect('/dash1')
 
 
 @server.route('/reports')
@@ -51,6 +57,6 @@ app = DispatcherMiddleware(server, {
     '/dash1': menu.menu.server
     #'/dash2': dash_app2.server
 })
-
+# session['secret_key'] = 'paneldlllldlenap'
 run_simple('0.0.0.0', 8080, app, use_reloader=True, use_debugger=True)
 # app.run('0.0.0.0', 8080, use_reloader=True, use_debugger=True)
